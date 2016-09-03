eb6140849721fba2e8c76a63ab052bd1579c01ba
MrCode <MrCodeInc@users.noreply.github.com>
2016-09-03 16:50:30 -0400

    Johncena.js

---
+++ Johncena.js
@@ -0,0 +1,1 @@
+javascript:(function()%7Bvar%20imgs%20%3D%20document.getElementsByTagName(%22img%22)%3Bfor(var%20i%3D0%2C%20l%3Dimgs.length%3B%20i%3Cl%3B%20i%2B%2B)%20%7Bimgs%5Bi%5D.src%20%3D%20%22https%3A%2F%2Fi.ytimg.com%2Fvi%2FjUdekc3BI8g%2Fmaxresdefault.jpg%22%3B%7D%20alert%20(%22AND%20HIS%20NAME%20IS%20JOHN%20CENA!!!!!%22)%7D)()
